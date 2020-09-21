91ebf972b010a54226e0daae394540d02fc59aac
sreesangameera <sreesangameera24@gmail.com>
2020-09-21 11:26:21 +0530

    Implemented changes

--- stash/Document.java
+++ stash/Document.java
@@ -1,5 +1,50 @@
 public class Document
 {
-	
+	private String name;
+	private String extension;
+	private String content;
+	private int id;
+
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+
+	 public void setExtension(String extension)
+        {
+                this.extension=extension;
+        }
+
+	 public void setContent(String content)
+        {
+                this.content=content;
+        }
+
+	 public void setId(int id)
+        {
+                this.id=id;
+        }
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getExtension()
+        {
+                return extension;
+        }
+
+	public String getContent()
+        {
+                return content;
+        }
+
+	public String getId()
+        {
+                return id;
+        }
+
+
 	
 }
